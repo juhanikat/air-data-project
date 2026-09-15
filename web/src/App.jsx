6855e77809c9e92539e1cfcabfc891fb5e0d4ce6
Jaakko Airikkala <jaakkel@gmail.com>
2026-09-15 16:25:40 +0300

    Add a chart to frontend

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,4 +1,13 @@
 import { useState, useEffect } from 'react'
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from 'recharts'
 import dataService from './services/air-data'
 
 const formatTimestamp = (timestamp) => {
@@ -12,7 +21,14 @@ const App = () => {
   const [data, setData] = useState([])
 
   useEffect(() => {
-    dataService.getData().then((data) => setData(data))
+    dataService.getData().then((data) =>
+      setData(
+        data.map((item) => ({
+          ...item,
+          time: formatTimestamp(item.timestamp),
+        })),
+      ),
+    )
   }, [])
 
   console.log(data)
@@ -20,10 +36,34 @@ const App = () => {
   return (
     <div>
       <h1>Ebin Air Data Broject</h1>
+
+      <ResponsiveContainer width="100%" height={500}>
+        <LineChart
+          data={data}
+          margin={{ top: 5, right: 20, left: 10, bottom: 100 }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis
+            dataKey="time"
+            angle={-45}
+            textAnchor="end"
+            interval="preserveStart"
+          />
+          <YAxis />
+          <Tooltip />
+          <Line
+            type="monotone"
+            dataKey="temperature"
+            stroke="#8884d8"
+            dot={false}
+          />
+        </LineChart>
+      </ResponsiveContainer>
+
+      <h2>Data</h2>
       {data.map((item) => (
         <div key={item.id}>
-          Time: {formatTimestamp(item.timestamp)} | Temperature:{' '}
-          {item.temperature}
+          {item.time} | Temperature: {item.temperature}
         </div>
       ))}
     </div>
